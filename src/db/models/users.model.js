9195e4387fd17383d10919793d8d853afd7d0cb2
Lean Gomez <gomezleandro149@gmail.com>
2023-06-27 18:13:25 -0300

    First commit

---
+++ src/db/models/users.model.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const usersSchema = new mongoose.Schema({
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  age: { type: Number },
+  password: { type: String },
+});
+
+export const usersModel = mongoose.model("Users", usersSchema);
